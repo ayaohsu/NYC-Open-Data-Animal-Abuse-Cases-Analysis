78d3af8fbc636205c641391170311064e94c9297
Tsung-Yao Hsu <ayao780607@gmail.com>
2022-12-06 12:17:28 +0800

    join dates with requests

--- transform_requests.py
+++ transform_requests.py
@@ -3,7 +3,7 @@ import sys
 
 from pyspark.sql import SparkSession
 
-from pyspark.sql.functions import col, row_number, lit, to_date, date_format
+from pyspark.sql.functions import col, row_number, lit, to_date, date_format, year, month, dayofyear
 from pyspark.sql.window import Window
 from pyspark.sql.types import IntegerType
 
@@ -33,14 +33,19 @@ def create_dim_date_table(sparkSession, requests):
     all_dates.show()
 
     dates_with_key = all_dates.withColumn("date_key", date_format(col("date"), "yyyyMMdd").cast(IntegerType()))
-    dates_with_key.createOrReplaceTempView("dim_date_type")
-    dates_with_key.show()    
+    dates_with_year_month_day = dates_with_key\
+        .withColumn("year", year(col("date")))\
+        .withColumn("month", month(col("date")))\
+        .withColumn("dayofyear", dayofyear(col("date")))
+
+    dates_with_year_month_day.createOrReplaceTempView("dim_date_type")
+    dates_with_year_month_day.show()    
 
 def create_fact_service_requests_table(sparkSession, requests):
     requests_with_desired_fields = requests\
         .select(col("unique_key"),\
-            col("created_date"),\
-            col("closed_date"),\
+            col("created_date").alias("created_date_time"),\
+            col("closed_date").alias("closed_date_time"),\
             col("complaint_type"),\
             col("incident_zip"),\
             col("address_type"),\
@@ -55,16 +60,26 @@ def create_fact_service_requests_table(sparkSession, requests):
             col("intersection_street_2")\
         )
     
-    complaint_types = sparkSession.sql("SELECT * FROM dim_complaint_type")
+    requests_with_date_as_timestamps = requests_with_desired_fields\
+        .withColumn("created_date", to_date(col("created_date_time")))\
+        .withColumn("closed_date", to_date(col("closed_date_time")))\
+        .drop("created_date_time", "closed_date_time")
     
-    requests_with_complaint_type_keys = requests_with_desired_fields\
-        .join(complaint_types, requests_with_desired_fields["complaint_type"] == complaint_types["complaint_type_name"])\
-        .select(requests_with_desired_fields["*"], complaint_types["complaint_type_key"])\
+    complaint_types = sparkSession.sql("SELECT complaint_type_key, complaint_type_name FROM dim_complaint_type")
+    
+    requests_with_complaint_type_keys = requests_with_date_as_timestamps\
+        .join(complaint_types, requests_with_date_as_timestamps["complaint_type"] == complaint_types["complaint_type_name"])\
+        .select(requests_with_date_as_timestamps["*"], complaint_types["complaint_type_key"])\
         .drop("complaint_type")
-    
-    requests_with_complaint_type_keys.createOrReplaceTempView("fact_service_requests")
 
-    requests_with_complaint_type_keys.show()
+    dates = sparkSession.sql("SELECT date_key, date FROM dim_date_type")
+    requests_with_date_key = requests_with_complaint_type_keys\
+        .join(dates, requests_with_complaint_type_keys["created_date"] == dates["date"])\
+        .select(requests_with_complaint_type_keys["*"], dates["date_key"].alias("created_date_key"))\
+        .drop("created_date")
+    
+    requests_with_date_key.createOrReplaceTempView("fact_service_requests")
+    requests_with_date_key.show()
 
 def transform_311_requests(sparkSession):
     s3_uri = f"s3://{S3_BUCKET_NAME}/{S3_FILE_NAME}"
